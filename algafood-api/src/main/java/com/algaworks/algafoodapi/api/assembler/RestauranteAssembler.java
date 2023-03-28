0065fe184d990666e057d707b4776b346263ede7
Vitor Silva <vitor.azevedo009@gmail.com>
2023-03-28 12:17:54 -0300

    Removing Lombok and ModelMapper dependencies

--- algafood-api/src/main/java/com/algaworks/algafoodapi/api/assembler/RestauranteAssembler.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/api/assembler/RestauranteAssembler.java
@@ -3,7 +3,6 @@ package com.algaworks.algafoodapi.api.assembler;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
