a656056a5d978cf7708c1300458eb44b2d5a965d
Vitor Silva <vitor.azevedo009@gmail.com>
2023-04-26 14:35:42 -0300

    Fiz query named Restaurante.consultarPorNome

--- algafood-api/src/main/java/com/algaworks/algafoodapi/domain/repository/RestauranteRepository.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/domain/repository/RestauranteRepository.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafoodapi.domain.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,8 @@ import com.algaworks.algafoodapi.domain.model.Restaurante;
  * RestauranteRepository
  */
 @Repository
-public interface RestauranteRepository extends JpaRepository<Restaurante, Long>{}
+public interface RestauranteRepository extends JpaRepository<Restaurante, Long> {
+
+  Page<Restaurante> consultarPorNome(String nome, Long id);
+
+}
