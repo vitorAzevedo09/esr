bf1d6919c092f72373dc30005507c3cd6be85f68
Vitor Silva <vitor.azevedo009@gmail.com>
2023-08-23 09:54:59 -0300

    UsuarioService Done

--- algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Usuario.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/domain/model/Usuario.java
@@ -21,76 +21,82 @@ import org.hibernate.annotations.CreationTimestamp;
 @Entity
 public class Usuario {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-  @Column(nullable = false)
-  private String nome;
+    @Column(nullable = false)
+    private String nome;
 
-  @Column(nullable = false)
-  private String email;
+    @Column(nullable = false)
+    private String email;
 
-  @Column(nullable = false)
-  private String senha;
+    @Column(nullable = false)
+    private String senha;
 
-  @CreationTimestamp
-  @Column(nullable = false, columnDefinition = "datetime")
-  private LocalDateTime dataCadastro;
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataCadastro;
 
-  /**
-   *
-   */
-  @ManyToMany
-  @JoinTable(name = "usuario_grupo", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "grupo_id"))
-  private List<Grupo> grupos = new ArrayList<>();
+    @ManyToMany
+    @JoinTable(name = "usuario_grupo", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "grupo_id"))
+    private List<Grupo> grupos = new ArrayList<>();
 
-  public List<Grupo> getGrupos() {
-    return grupos;
-  }
+    public List<Grupo> getGrupos() {
+        return grupos;
+    }
 
-  public void setGrupos(List<Grupo> grupos) {
-    this.grupos = grupos;
-  }
+    public void setGrupos(List<Grupo> grupos) {
+        this.grupos = grupos;
+    }
 
-  public Long getId() {
-    return id;
-  }
+    public Long getId() {
+        return id;
+    }
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-  public String getNome() {
-    return nome;
-  }
+    public String getNome() {
+        return nome;
+    }
 
-  public void setNome(String nome) {
-    this.nome = nome;
-  }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public String getSenha() {
-    return senha;
-  }
+    public String getSenha() {
+        return senha;
+    }
 
-  public void setSenha(String senha) {
-    this.senha = senha;
-  }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
 
-  public LocalDateTime getDataCadastro() {
-    return dataCadastro;
-  }
+    public LocalDateTime getDataCadastro() {
+        return dataCadastro;
+    }
 
-  public void setDataCadastro(LocalDateTime dataCadastro) {
-    this.dataCadastro = dataCadastro;
-  }
+    public void setDataCadastro(LocalDateTime dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+
+	public boolean senhaCoincideCom(String senha) {
+		return getSenha().equals(senha);
+	}
+	
+	public boolean senhaNaoCoincideCom(String senha) {
+		return !senhaCoincideCom(senha);
+	}
 
 }
---
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/domain/service/UsuarioService.java
@@ -0,0 +1,37 @@
+package com.algaworks.algafoodapi.domain.service;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.algaworks.algafoodapi.domain.model.Usuario;
+import com.algaworks.algafoodapi.domain.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+
+    @Transactional
+    public void alterarSenha(Long usuarioID, String senhaAtual, String novaSenha) {
+        Usuario usuario = buscarOuFalhar(usuarioID);
+        if(usuario.senhaNaoCoincideCom(senhaAtual)){
+			throw new RuntimeException("Senha atual informada não coincide com a senha do usuário.");
+        }
+    }
+
+    @Transactional
+    public Usuario buscarOuFalhar(Long usuarioID) {
+        return usuarioRepository.findById(usuarioID)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+}
