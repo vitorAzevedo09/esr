bc4b7dd0ff29a4fa7efbe2668be86545947ae08c
Vitor Silva <vitor.azevedo009@gmail.com>
2023-10-25 13:17:04 -0300

    Added bean validation package and configured it

--- algafood-api/src/main/java/com/algaworks/algafoodapi/api/config/SpringFoxConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafoodapi/api/config/SpringFoxConfig.java
@@ -2,12 +2,16 @@ package com.algaworks.algafoodapi.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 
 @Configuration
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SpringFoxConfig {
   @Bean
   public Docket api() {
